2a3af7e31c484c74afb84a9cee39295236665a1d
uf-code <ugur.firat.developer@gmail.com>
2021-03-24 10:14:28 +0300

    configuration for the firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD7OvHrZznx695Gn6j3m9fT5fVpiMrx_zQ',
+  authDomain: 'udemy-vue-firebase-sites-70910.firebaseapp.com',
+  projectId: 'udemy-vue-firebase-sites-70910',
+  storageBucket: 'udemy-vue-firebase-sites-70910.appspot.com',
+  messagingSenderId: '679903118906',
+  appId: '1:679903118906:web:884481d9e4356a54a89703',
+}
+
+//init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
