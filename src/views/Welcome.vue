90c81ffa6c7064148de31a3d04227a62a57eaf1f
uf-code <ugur.firat.developer@gmail.com>
2021-03-27 20:28:39 +0300

    chatroom component is created and implemented and user signup and login is implemented to chatroom

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -3,14 +3,14 @@
     <p>Welcome</p>
     <div v-if="showLogin">
       <h2>Login</h2>
-      <LoginForm />
+      <LoginForm @login="enterChat" />
       <p>
         No account yet? <span @click="showLogin = false">Signup</span> instead
       </p>
     </div>
     <div v-else>
       <h2>Signup</h2>
-      <SignupForm />
+      <SignupForm @signup="enterChat" />
       <p>
         Already registered? <span @click="showLogin = true">Login</span> instead
       </p>
@@ -23,13 +23,18 @@ import SignupForm from '../components/SignupForm'
 import LoginForm from '../components/LoginForm'
 
 import { ref } from 'vue'
-
+import { useRouter } from 'vue-router'
 export default {
   components: { SignupForm, LoginForm },
   setup() {
     const showLogin = ref(true)
+    const router = useRouter()
 
-    return { showLogin }
+    const enterChat = () => {
+      router.push({ name: 'Chatroom' })
+    }
+
+    return { showLogin, enterChat }
   },
 }
 </script>
