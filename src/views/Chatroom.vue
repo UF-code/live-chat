55b4e2e13f92138a5bf79225ae3a88bb93a99d48
uf-code <ugur.firat.developer@gmail.com>
2021-03-30 10:57:53 +0300

    newchatform component is created and integrated to chatroom v integrated to chatroom

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="container">
     <Navbar />
+    <NewChatForm />
   </div>
 </template>
 
 <script>
 import { watch } from 'vue'
 import { useRouter } from 'vue-router'
+import NewChatForm from '../components/NewChatForm'
 import Navbar from '../components/Navbar'
 import getUser from '../composables/getUser'
 
 export default {
-  components: { Navbar },
+  components: { Navbar, NewChatForm },
   setup() {
     const { user } = getUser()
     const router = useRouter()
